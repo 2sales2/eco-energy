9be5aeb8b902cc5facd10f3c8279c8372c782aed
2sales2 <pedrosalesna@gmail.com>
2026-06-04 18:18:04 -0300

    persistence.xml

---
+++ ecoenergy/src/main/java/com/ecoenergy/App.java
@@ -0,0 +1,13 @@
+package com.ecoenergy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
